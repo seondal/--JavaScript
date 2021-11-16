7c46d46a4ec39fb66026aeeb13d115b472021210
seondal <sseondal@gmail.com>
2021-11-16 09:57:12 +0900

    3일차 : HTML + Javascript (2)

---
+++ Day03_삼행시/index.js
@@ -0,0 +1,14 @@
+function first() {
+  var text = document.getElementsByClassName("first");
+  text[0].innerHTML = "모처럼이니";
+}
+
+function second() {
+  var text = document.getElementsByClassName("second");
+  text[0].innerHTML = "각잡고 하면";
+}
+
+function third() {
+  var text = document.getElementsByClassName("third");
+  text[0].innerHTML = "코린이 탈출이다";
+}
